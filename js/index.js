448a90464e9a60d5f61ab8231c181e46f163bf2f
Fiery-Mouwnies <166019254+Fiery-Mouwnies@users.noreply.github.com>
2025-09-25 00:55:40 +0200

    Updated index.html

--- js/index.js
+++ js/index.js
@@ -1,16 +1,6 @@
-// Load GSAP plugins
-gsap.registerPlugin(Flip, ScrollTrigger);
+// Import the necessary function for preloading images
+import { preloadImages } from './utils.js';
 
-// Load GSAP plugins
-gsap.registerPlugin(Flip, ScrollTrigger);
-
-// Preload images function
-const preloadImages = (selector = 'img') => {
-    return new Promise((resolve) => {
-        imagesLoaded(document.querySelectorAll(selector), { background: true }, resolve);
-    }
-    )
-}
 // Define a variable that will store the Lenis smooth scrolling object
 let lenis;
 
